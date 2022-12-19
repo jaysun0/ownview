4243ad20e62ac08146c41caebdaaeead16d9ebea
Daniil Kochkin <danyakocha@yandex.ru>
2022-12-19 21:30:01 +0300

    fix: images in gallery load faster after deleting backdrop-filter(blur)

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 import setupEventListeners from './components/events.js';
 setupEventListeners();
 
-
 /* TO BE DONE:
 
-* ? add loader for images in gallery
-* ? change design a bit (may be some elements position)
+* ? change design a bit (maybe some elements position)
 * indicators for lots of photos
 * add apps description
 * add contacts
