351252f67d33cfac50512159e78c0a611cba6d41
Daniil <jaysun0@yandex.ru>
2022-12-21 19:32:21 +0300

    feat: deleted tasks from the list (done in previos commit)

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ setupEventListeners();
 
 /* TO BE DONE:
 
-* fix appeared bug with images-order in gallery
-* refactor new code changes (fast gallery sliding)
 * ? change design a bit (maybe some elements position)
 * indicators for lots of photos
 * ??? integrate image search?
